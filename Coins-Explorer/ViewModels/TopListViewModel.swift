d80b19f8e74d1247935aeb9fc6771ce25a63361c
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2020-08-23 10:54:06 +0300

    feat: add top list network call to top list view model

--- Coins-Explorer/ViewModels/TopListViewModel.swift
+++ Coins-Explorer/ViewModels/TopListViewModel.swift
@@ -8,9 +8,34 @@
 import Foundation
 import Combine
 import CryptoCompareKit
+import CombineSchedulers
 
 final class TopListViewModel: ObservableObject {
-    init() {
+    
+    @Published var topListData: [TopListData] = []
+    
+    private let client: CryptoCompareClient
+    private let scheduler: AnySchedulerOf<DispatchQueue>
+    
+    private var clientCancellable: AnyCancellable? {
+        willSet {
+            clientCancellable?.cancel()
+        }
+    }
+    
+    init(client: CryptoCompareClient = .live,
+         scheduler: AnySchedulerOf<DispatchQueue> = DispatchQueue.main.eraseToAnyScheduler()) {
+        self.client = client
+        self.scheduler = scheduler
         
+        fetchTopList(limit: 20, tsym: "USD")
+    }
+    
+    func fetchTopList(limit: Int, tsym: String) {
+        clientCancellable = client.topList(limit, tsym)
+            .replaceError(with: TopList())
+            .receive(on: scheduler)
+            .map { $0.data }
+            .sink(receiveValue: { [weak self] in self?.topListData = $0 })
     }
 }
