7d6c685cd044078b6421f58b325d6509fea7b044
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2020-08-23 09:21:52 +0300

    Initial Commit

---
+++ Coins-Explorer/Coins_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coins_ExplorerApp.swift
+//  Coins-Explorer
+//
+//  Created by Spiros Gerokostas on 23/8/20.
+//
+
+import SwiftUI
+
+@main
+struct Coins_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
