e9bc5a2ef427f456d765a651451ce91eea9d8864
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2020-08-30 20:02:59 +0300

    feat: add image view component

--- Coins-Explorer/Views/ImageView.swift
+++ Coins-Explorer/Views/ImageView.swift
@@ -5,16 +5,28 @@
 //  Created by Spiros Gerokostas on 23/8/20.
 //
 
+import SDWebImageSwiftUI
 import SwiftUI
 
 struct ImageView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  let path: String?
+  let size: CGFloat
+
+  var body: some View {
+    WebImage(url: path != nil ? URL(string: path!) : nil)
+      .resizable()
+      .renderingMode(.original)
+      .indicator(.activity)
+      .animation(.easeInOut(duration: 0.2))
+      .transition(.fade)
+      .aspectRatio(contentMode: .fit)
+      .frame(width: size, height: size)
+      .animation(.spring())
+  }
 }
 
 struct ImageView_Previews: PreviewProvider {
-    static var previews: some View {
-        ImageView()
-    }
+  static var previews: some View {
+    ImageView(path: "", size: 20)
+  }
 }
